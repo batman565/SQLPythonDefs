42bc0e2d9aa6e64b3ca94c9c85972f00d2ce224a
Дремов Михаил Игоревич <41387488+batman565@users.noreply.github.com>
2024-12-02 01:15:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,27 @@ def insertphone(cursor, idclient, phone):
                              values(%s ,%s)""", (idclient, phone,))
 
 
-def updateclient(cursor, ids, name, surname, email):
-    return cursor.execute("""UPDATE client SET name = %s, 
-                             surname = %s, 
-                             email = %s WHERE id = %s""", (name, surname, email, ids,))
+def updateclient(cursor, ids, name=None, surname=None, email=None):
+
+    query = "UPDATE client SET "
+    params = []
+
+    if name is not None:
+        query += "name = %s, "
+        params.append(name)
+    if surname is not None:
+        query += "surname = %s, "
+        params.append(surname)
+    if email is not None:
+        query += "email = %s, "
+        params.append(email)
+
+    query = query.rstrip(", ")
+
+    query += " WHERE id = %s"
+    params.append(ids)
+
+    cursor.execute(query, tuple(params))
 
 
 def deletenumber(cursor, idclient, phone):
@@ -27,14 +44,33 @@ def deletenumber(cursor, idclient, phone):
 
 
 def deleteclient(cursor, ids):
-    return cursor.execute("""delete from client where id = %s""", (ids,))
+    cursor.execute("""select idclient from phone where idclient = %s""", (ids,))
+    if cursor.fetchone() is not None:
+        return print("Невозможно удалить клиента так как к нему привязан номер телефона")
+    else:
+        return cursor.execute("""delete from client where id = %s""", (ids,))
 
 
-def findclient(cursor, name, surname, email, phone):
-    cursor.execute("""select c.id, name, surname, email from client c
-                             left join phone p on p.idclient = c.id
-                             where name like %s and surname like %s and email like %s
-                             or phone like %s""", (name, surname, email, phone,))
+def findclient(cursor, name=None, surname=None, email=None, phone=None):
+    query = """SELECT c.id, name, surname, email FROM client c
+               LEFT JOIN phone p ON p.idclient = c.id
+               WHERE 1=1"""
+    params = []
+
+    if name is not None:
+        query += " AND name LIKE %s"
+        params.append(f"%{name}%")
+    if surname is not None:
+        query += " AND surname LIKE %s"
+        params.append(f"%{surname}%")
+    if email is not None:
+        query += " AND email LIKE %s"
+        params.append(f"%{email}%")
+    if phone is not None:
+        query += " AND phone LIKE %s"
+        params.append(f"%{phone}%")
+
+    cursor.execute(query, tuple(params))
     return print(cursor.fetchall())
 
 
@@ -51,17 +87,16 @@ configcreatephone = """CREATE TABLE IF NOT EXISTS Phone(
                     );"""
 
 
-conn = psycopg2.connect(database="ClientsDB", user="postgres", password="")
-with conn.cursor() as cur:
-    created_data_base(cur, configcreateclient)
-    created_data_base(cur, configcreatephone)
-    insertclient(cur, 'Михаил', 'Дремов', 'mixail_dremov@mail.ru')
-    insertphone(cur, 1, '89850395812')
-    updateclient(cur, 1, 'Антон', 'Михайлов', 'anton_m@mail.ru')
-    deletenumber(cur, 1, '89850395812')
-    deleteclient(cur, 1)
-    insertclient(cur, 'Михаил', 'Дремов', 'mixail_dremov@mail.ru')
-    insertphone(cur, 2, '89850395812')
-    conn.commit()
-    findclient(cur, '%%', '%%', '%%', '89850395812')
-conn.close()
+if __name__ == "__main__":
+    with psycopg2.connect(database="ClientsDB", user="postgres", password="") as con:
+        with con.cursor() as cur:
+            created_data_base(cur, configcreateclient)
+            created_data_base(cur, configcreatephone)
+            insertclient(cur, 'Михаил', 'Дремов', 'mixail_dremov@mail.ru')
+            insertphone(cur, 1, '89850395812')
+            updateclient(cur, 2, 'Антон', None, 'anton_m@mail.ru')
+            deletenumber(cur, 1, '89850395812')
+            deleteclient(cur, 1)
+            insertclient(cur, 'Михаил', 'Дремов', 'mixail_dremov@mail.ru')
+            insertphone(cur, 2, '89850395812')
+            findclient(cur, None, None, "Дремов", None)
